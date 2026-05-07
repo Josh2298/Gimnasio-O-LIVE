9b8af425933f0470f8a357f6f71fadfabdb02868
Josh2298 <edward98jm@gmail.com>
2026-05-07 12:20:34 -0400

    ajuestes a membresia

---
+++ database/migrations/2026_05_07_155555_update_membresias_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('membresias', function (Blueprint $table) {
+            // PROMOCION
+            $table->unsignedBigInteger('promocion_id')
+            ->nullable()
+            ->after('user_id');
+
+            // PERSONAS
+            $table->integer('cantidad_personas')
+            ->default(1)
+            ->after('disciplina');
+
+            // FOREIGN KEY
+            $table->foreign('promocion_id')
+            ->references('id')
+            ->on('promociones')
+            ->onDelete('set null');
+    });
+        
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
